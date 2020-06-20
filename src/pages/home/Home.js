460241640cfb4fcb1c395eb231b4f3f6ea4d91ea
VitalyZibulski <laravel.vitaly.zibulski2017@gmail.com>
2020-06-21 00:11:28 +0300

    add component exchange, homepage

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import './home.scss';
+import {Exchange} from "../../components/exchange/Exchange";
 
 export const Home = () => {
   return (
-   <h1>Home Page</h1>
+   <Exchange />
   )
 }
