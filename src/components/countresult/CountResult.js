41c0ec579b52da0d514ba2823e814928b486f6c1
VitalyZibulski <laravel.vitaly.zibulski2017@gmail.com>
2020-06-24 09:35:03 +0300

    add calculator

---
+++ src/components/countresult/CountResult.js
@@ -0,0 +1,21 @@
+import React, {useContext} from 'react';
+import './countResult.scss';
+import {RateContext} from "../../context/RateContext";
+
+export const CountResult = () => {
+  const {state} = useContext(RateContext)
+
+  return (
+    <div className="calcResult">
+      <ul>
+        <li>
+          <p>
+            <span>{state.inputValue}&nbsp;RUB</span>
+            =
+            <span>{state.result}&nbsp;{state.currencyValue}</span>
+          </p>
+        </li>
+      </ul>
+    </div>
+  )
+}
