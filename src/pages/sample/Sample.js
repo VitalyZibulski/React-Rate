0263f71b6e591c6e6acb8b7bf7f7e4255cb39ab9
VitalyZibulski <laravel.vitaly.zibulski2017@gmail.com>
2020-06-30 09:34:53 +0300

    add save to database

--- src/pages/sample/Sample.js
+++ src/pages/sample/Sample.js
@@ -1,55 +1,57 @@
-import React, {useContext} from 'react';
-import './sample.scss';
-import {RateContext} from "../../context/RateContext";
-import {Button} from "../../components/button/Button";
+import React, {useContext} from 'react'
+import './sample.scss'
+import {RateContext} from '../../context/RateContext';
+import {Button} from '../../components/button/Button';
 
 export const Sample = () => {
-  const {
-    state,
-    baseHandler,
-    base2Handler,
-    sampleDateHandler,
-    dataWrite
-  } = useContext(RateContext)
+  const {state, baseHandler, base2Handler, sampleDateHandler, dataWrite} = useContext(RateContext)
 
   return (
-   <div className="sample">
-     <div className="sample-container">
-       <div>
-         <h3>
+    <div className='sample'>
+      <div className='sampleContainer'>
+
+        <div>
+          <h3>
             Получить курс: &nbsp;
+            <select onChange={baseHandler} value={state.sample.base}>
+              {Object.keys(state.currency).map((item, i) => {
+                return (
+                  <option key={item}>{item}</option>
+                )
+              })}
+            </select>
+            &nbsp; &nbsp;  к  &nbsp; &nbsp;
+            <select onChange={base2Handler} value={state.sample.base2}>
+              {Object.keys(state.currency).map((item, i) => {
+                return (
+                  <option key={item}>{item}</option>
+                )
+              })}
+            </select>
+          </h3>
+        </div>
 
-           <select onChange={baseHandler} value={state.sample.base}>
-             {
-               Object.keys(state.currency).map((item, i) => {
-                 return (
-                   <option key={item}>{item}</option>
-                 )
-               })
-             }
-           </select>
-            &nbsp; &nbsp; к &nbsp;&nbsp;
-           <select onChange={base2Handler} value={state.sample.base2}>
-             {
-               Object.keys(state.currency).map((item, i) => {
-                 return (
-                   <option key={item}>{item}</option>
-                 )
-               })
-             }
-           </select>
-         </h3>
-       </div>
-       <div className="sampleHead">
-         <span>Дата: <input type="date" onChange={sampleDateHandler}/></span>
-         <Button text="Получить курс" click={dataWrite} arg={state.sample}/>
-       </div>
-       <div className="sampleResult">
+        <div className='sampleHead'>
+          <span>Дата: <input type='date' onChange={sampleDateHandler}/> </span>
+          <Button text='Получить курс' click={dataWrite} arg={state.sample}/>
+        </div>
+
+        <div className='sampleResult'>
           <ul>
-
+            {Object.keys(state.sampleList).map((item, i) => {
+              return (
+                <li key={item}>
+                  <span><img src={state.currency[state.sampleList[item].base].flag}
+                             alt={item}/>&nbsp;{state.sampleList[item].base}</span>
+                  <span>{state.sampleList[item].date}</span>
+                  <span>{`${state.sampleList[item].course} ${state.sampleList[item].base2}`}</span>
+                  <button><i className='fa fa-times'/></button>
+                </li>
+              )
+            })}
           </ul>
-       </div>
-     </div>
-   </div>
+        </div>
+      </div>
+    </div>
   )
 }
